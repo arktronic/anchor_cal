93974d8a940a377a194e727ca70c0084444b0e53
Sasha Kotlyar <arktronic@users.noreply.github.com>
2026-08-10 13:28:54 -0400

    Clear

--- debug_deploy.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /value') do set datetime=%%I
-set BUILD_DATE=%datetime:~0,12%
-echo Build date: %BUILD_DATE%
-flutter build apk --debug --dart-define=BUILD_DATE=%BUILD_DATE% && adb install build\app\outputs\flutter-apk\app-debug.apk
--- deploy.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-flutter build apk --release && adb install build\app\outputs\flutter-apk\app-release.apk
